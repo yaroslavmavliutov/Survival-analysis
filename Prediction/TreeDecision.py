2f463f2c14097ec4c6b07ad2367e69213bf546de
yaroslavmavliutov <yaroslav.mavliutov@gmail.com>
2018-06-08 17:47:15 +0300

    Update

--- Prediction/TreeDecision.py
+++ Prediction/TreeDecision.py
@@ -3,7 +3,7 @@ from sklearn.tree import *
 import matplotlib.pyplot as plt
 import numpy as np
 
-def predict():
+def predict(data):
     dataset = pars()
     y = (dataset['Вірусний агент']).as_matrix()
     del dataset['Вірусний агент']
@@ -12,25 +12,7 @@ def predict():
     clf = DecisionTreeClassifier()
     clf = clf.fit(X, y)
 
-    # ПРОГНОЗУЮ
-    # ['Вікова група', 'Температура тіла\nдо лікування', 'Характер мокроти\nдо лікування',
-    #  'Загальний стан хворого\nдо лікування',
-    #  'Локалізація НП\nдо лікування', 'Рентгенодинаміка\nдо лікування']
-
-    # age = input('Вікова група(1: 18-30, 2: 31-60, 3: >61): ')
-    # temp = input('Температура(0: <37°C, 1: 37-38°C, 3: >38°C): ')
-    # mokrora = input('Характер мокроти(0: нема, 1: слизова, 2: слизово-гнійна, 3: гнійна): ')
-    # zagaln = input('Загальний стан(1: задовільний, 2: середній, 3: важкий): ')
-    # lokal = input('Локалізація(0: нема, 1: однобічна, 2: двобічна): ')
-    # rentgen = input('Рентгенодинаміка(1: повне розсмоктування, 2: часткове, 3: без динаміки, 4: від\'ємна динаміка): ')
-    age = 1
-    temp = 1
-    mokrora = 0
-    zagaln = 2
-    lokal = 1
-    rentgen = 1
-
-    p = clf.predict_proba([[age, temp, mokrora, zagaln, lokal, rentgen]])[0]
+    p = clf.predict_proba([[data[0], data[1], data[2], data[5], data[3], data[4]]])[0]
 
     x = p.tolist()
     plt.bar(np.arange(len(x)), height=x)
